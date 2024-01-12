485b5c547743272f658ae95651a4192497b177b2
atta jnr <attajnr731@gmail.com>
2024-01-12 12:53:40 +0000

    db added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(cors());
+connectDB();
 
 app.listen(port, () => {
   console.log("listening on port", port);
 });
- 
